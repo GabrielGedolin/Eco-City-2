e229c06a598be34c25e78247e13b935beca74a72
GahCyber <gabrielgedoli@gmail.com>
2025-03-16 10:26:51 -0300

    teste de segurança

--- pages/api/middeware/auth.ts
+++ pages/api/middeware/auth.ts
@@ -1,27 +1,32 @@
-// pages/api/middleware/auth.ts
 import { NextApiRequest, NextApiResponse } from 'next';
 import jwt from 'jsonwebtoken';
 
-const JWT_SECRET = process.env.JWT_SECRET || 'sua_chave_secreta'; // Use a mesma chave usada no login
+const JWT_SECRET = process.env.JWT_SECRET || 'sua_chave_secreta';
 
-export default function authMiddleware(handler: (req: NextApiRequest, res: NextApiResponse) => Promise<void>) {
-    return async (req: NextApiRequest, res: NextApiResponse) => {
-        // Verifica se o token está presente no cabeçalho da requisição
-        const token = req.headers.authorization?.split(' ')[1]; // Formato: "Bearer <token>"
+// Estende o tipo NextApiRequest para incluir a propriedade userId
+interface ExtendedNextApiRequest extends NextApiRequest {
+  headers: any;
+  userId?: any;
+}
 
-        if (!token) {
-            return res.status(401).json({ error: 'Token de autenticação não fornecido.' });
-        }
+export default function authMiddleware(handler: (req: ExtendedNextApiRequest, res: NextApiResponse) => Promise<void>) {
+  return async (req: ExtendedNextApiRequest, res: NextApiResponse) => {
+    // Verifica se o token está presente no cabeçalho da requisição
+    const token = req.headers.authorization?.split(' ')[1]; // Formato: "Bearer <token>"
 
-        try {
-            // Verifica se o token é válido
-            const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
-            // Adiciona o ID do usuário à requisição para uso posterior
-            req.userId = decoded.userId;
-            // Chama o handler da rota protegida
-            return handler(req, res);
-        } catch (err) {
-            return res.status(401).json({ error: 'Token inválido ou expirado.' });
-        }
-    };
+    if (!token) {
+      return res.status(401).json({ error: 'Token de autenticação não fornecido.' });
+    }
+
+    try {
+      // Verifica se o token é válido
+      const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+      // Adiciona o ID do usuário à requisição para uso posterior
+      req.userId = decoded.userId; // Agora isso é permitido
+      // Chama o handler da rota protegida
+      return handler(req, res);
+    } catch (err) {
+      return res.status(401).json({ error: 'Token inválido ou expirado.' });
+    }
+  };
 }
