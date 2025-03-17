ba7b70592b0541bb9df3062fa1a188201e53ab99
GahCyber <gabrielgedoli@gmail.com>
2025-03-16 21:41:23 -0300

    Coloquei o analytics da vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import React from 'react';
 import "../public/style/style.css"
 import "../public/style/clash-display.css"
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
