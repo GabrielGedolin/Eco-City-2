396c85a0ecb1865668e39f8dc258c6e796527b35
Gustavo Alves <164105793+gualvesx@users.noreply.github.com>
2025-03-21 08:23:02 -0300

    Update admusers.tsx

--- pages/admusers.tsx
+++ pages/admusers.tsx
@@ -73,9 +73,9 @@ export default function LoginPage() {
             <main data-bs-spy="scroll" data-bs-target=".navbar">
                 <nav className="navbar navbar-expand-lg sticky-top bg-light bg-opacity-50">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">
-                            <img src="assets/images/logo.png" alt="" />
-                        </a>
+                        <Link href="/" passHref className="navbar-brand">
+                          <img src="/img/Logo1.png" alt="ECO CITY Logo" />
+                        </Link>
                         <h1>Dengue Zero</h1>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
