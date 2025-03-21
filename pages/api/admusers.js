fa323862fecbc1f8c7798a845fab50c666ff3089
Gustavo Alves <164105793+gualvesx@users.noreply.github.com>
2025-03-21 08:47:13 -0300

    Update admusers.js

--- pages/api/admusers.js
+++ pages/api/admusers.js
@@ -1,13 +1,13 @@
 import { neon } from '@neondatabase/serverless';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.DATABASE_URL);
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req, res) {
     if (req.method === 'GET') {
         try {
             const localizacoes = await sql`SELECT id, nome, email FROM localizacoes`;
-            res.status(200).json({ localizacoes });
+            res.status(200).json(localizacoes);
         } catch (err) {
             console.error('Erro ao buscar localizações:', err);
             res.status(500).json({ error: 'Erro ao buscar localizações.' });
