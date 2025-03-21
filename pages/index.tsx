363b8bee3cc24995e6d0e3248b7391a4807903ca
Gustavo Alves <164105793+gualvesx@users.noreply.github.com>
2025-03-21 08:35:50 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,21 +49,14 @@ const Home = () => {
       <main data-bs-spy="scroll" data-bs-target=".navbar">
         {/* NAVBAR */}
         <nav className="navbar navbar-expand-lg sticky-top bg-light bg-opacity-50">
-          <div className="container">
+          <div className="container-fluid">
             <Link href="/" passHref className="navbar-brand">
               <img src="/img/Logo1.png" alt="ECO CITY Logo" />
             </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
