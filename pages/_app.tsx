5dba000488161075bf86d12c9e6dba23e494a4d3
GahCyber <gabrielgedoli@gmail.com>
2025-03-27 06:27:01 -0300

    coloquei o Analitics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 import type { AppProps } from 'next/app';
 import React from 'react';
 import "../public/style/style.css"
-import "../public/style/clash-display.css"
 import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="">
+    <>
+      <Analytics />
+      <div className="">
       <main className="">
         <Component {...pageProps} />
       </main>
-    </div>
+      </div>
+    </>
   );
 }
 
